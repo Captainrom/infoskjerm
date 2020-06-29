51ced1548bc02c99aae2d272334e2d96123ab5d0
Magnus Skalleberg Rom <magnussrom@gmail.com>
2020-06-29 13:52:04 +0200

    Updates

---
+++ precache-manifest.653ce92d2601af32fa74f58281e3b411.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289ac321948063b19675364c88e9916f",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "362d7a16540e1db86037",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "785abccff1762f1ab6ab",
+    "url": "/infoskjerm/static/js/2.fc7bf9ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.fc7bf9ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362d7a16540e1db86037",
+    "url": "/infoskjerm/static/js/main.ea5cf021.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
